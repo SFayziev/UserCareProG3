6c8973aab8d76c68048e72ee87d461850e16c038
Faiziev Shuhrat <SFayziev@users.noreply.github.com>
2016-06-26 18:48:10 +0000

    Jenkins test 03

---
+++ src/test/java/com/sh/utils/TimeAgoTest.java
@@ -0,0 +1,22 @@
+package com.sh.utils;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lenovo on 26.06.2016.
+ */
+public class TimeAgoTest {
+
+    @Test
+    public void testGetI18nvalue() throws Exception {
+        System.out.println("TimeAgo class unitTest");
+        TimeAgo timeAgo = new TimeAgo(new Date());
+        assertNotNull(timeAgo.getI18nvalue());
+        assertNotNull(timeAgo.getAgovalue());
+    }
+
+}
