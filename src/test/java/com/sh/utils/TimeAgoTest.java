2f29f351f8c4bdaeab38729db7ceecd36e556e64
scob <scob@0e03f3e4-7e5c-e749-8ca2-0754f463caa0>
2016-06-27 05:36:16 +0000

    Jenkins test 03

--- src/test/java/com/sh/utils/TimeAgoTest.java
+++ src/test/java/com/sh/utils/TimeAgoTest.java
@@ -7,12 +7,14 @@ import java.util.Date;
 import static org.junit.Assert.*;
 
 /**
+ * TimeAgoTest UnitTest
  * Created by Lenovo on 26.06.2016.
  */
 public class TimeAgoTest {
 
     @Test
-    public void testGetI18nvalue() throws Exception {
+    public final void testGetI18nvalue() throws Exception {
+
         System.out.println("TimeAgo class unitTest");
         TimeAgo timeAgo = new TimeAgo(new Date());
         assertNotNull(timeAgo.getI18nvalue());
